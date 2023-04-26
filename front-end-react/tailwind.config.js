bfb7bc18a7b15bfd69f2864e04124591df01efc1
Andrea Barr <andrea@andreabarr.dev>
2023-04-25 21:32:08 -0500

    feature/tailwind-setup: initial setup of tailwind css

---
+++ front-end-react/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],  // this will probably change
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
