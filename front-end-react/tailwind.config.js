a835332c4be08d0fd187942c7efb43b07f6d71c4
Andrea Barr <andrea@andreabarr.dev>
2023-05-09 20:41:19 -0500

    feature/tailwind-setup: add very basic theme vars

--- front-end-react/tailwind.config.js
+++ front-end-react/tailwind.config.js
@@ -1,9 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],  // this will probably change
+  // directories/subdirectories and file types to scan for tw class names
+  // include template names only (.js, .html), never css files themselves
+  content: [
+    './src/**/*.{html,js,jsx,ts}',
+    './public/index.html',
+  ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   plugins: [],
 }
-
